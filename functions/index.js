68c8a91901d7c4b1be59c8d48385c602571a3546
gohana <gal.ohana@gmail.com>
2020-06-30 14:42:56 -0700

    working fix

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,47 @@
-const functions = require('firebase-functions');
-const { admin } = require('firebase-admin');
-//const {admin } = require('firebase-admin');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello QueuedUp");
- });
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
- /*exports.getPosts = functions.https.onRequest((request, response) => {
-     admin
-     .firestore()
-     .collection('posts')
-     .get()
-     .then((data) => {
+admin.initializeApp();
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello QueuedUp");
+});
+
+exports.getPosts = functions.https.onRequest((request, response) => {
+    admin
+        .firestore()
+        .collection("Posts")
+        .get()
+        .then((data) => {
             let posts = [];
-            data.forEach(doc => {
+
+            data.forEach((doc) => {
                 posts.push(doc.data());
-            })
+            });
+
             return response.json(posts);
         })
-        .catch(err => console.log(err));
- });
+        .catch((err) => console.log(err));
+});
 
- exports.createPost = functions.https.onRequest((request, response) => {
-   const newPost = {
-       body: request.body.body,
-       userHandle: request.body.userHandle,
-       createdAt: admin.firestore.Timestamp.fromDate(new Date())
-   };
+exports.createPost = functions.https.onRequest((request, response) => {
+    const newPost = {
+        body: request.body.body,
+        userHandle: request.body.userHandle,
+        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+    };
 
-   admin.firestore()
-   .collection('posts')
-   .add(newPost)
-   .then(doc => {
-       response.json ({message: `document ${doc.id} created successfully`});
-   })
-   .catch(err =>{
-       response.status(500).json({err : 'something went wrong'});
-       console.log(err);
-   });
-});*/
+    admin
+        .firestore()
+        .collection("Posts")
+        .add(JSON.parse(JSON.stringify(newPost)))
+        .then((doc) => {
+            response.json({
+                message: `successfully created document ${doc.id}`,
+            });
+        })
+        .catch((err) => {
+            response.status(500).json({ err: "error occurred!" });
+            console.error(err);
+        });
+});
