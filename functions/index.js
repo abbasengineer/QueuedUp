2b54a3b0bc849a4cfcc2287df9ce3f75cece0fd4
gohana <gal.ohana@gmail.com>
2020-06-30 20:39:09 -0700

    Posts --> post

--- functions/index.js
+++ functions/index.js
@@ -4,44 +4,44 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello QueuedUp");
+  response.send("Hello QueuedUp");
 });
 
 exports.getPosts = functions.https.onRequest((request, response) => {
-    admin
-        .firestore()
-        .collection("Posts")
-        .get()
-        .then((data) => {
-            let posts = [];
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then((data) => {
+      let posts = [];
 
-            data.forEach((doc) => {
-                posts.push(doc.data());
-            });
+      data.forEach((doc) => {
+        posts.push(doc.data());
+      });
 
-            return response.json(posts);
-        })
-        .catch((err) => console.log(err));
+      return response.json(posts);
+    })
+    .catch((err) => console.log(err));
 });
 
 exports.createPost = functions.https.onRequest((request, response) => {
-    const newPost = {
-        body: request.body.body,
-        userHandle: request.body.userHandle,
-        createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-    };
+  const newPost = {
+    body: request.body.body,
+    userHandle: request.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
 
-    admin
-        .firestore()
-        .collection("Posts")
-        .add(JSON.parse(JSON.stringify(newPost)))
-        .then((doc) => {
-            response.json({
-                message: `successfully created document ${doc.id}`,
-            });
-        })
-        .catch((err) => {
-            response.status(500).json({ err: "error occurred!" });
-            console.error(err);
-        });
+  admin
+    .firestore()
+    .collection("posts")
+    .add(JSON.parse(JSON.stringify(newPost)))
+    .then((doc) => {
+      response.json({
+        message: `successfully created document ${doc.id}`,
+      });
+    })
+    .catch((err) => {
+      response.status(500).json({ err: "error occurred!" });
+      console.error(err);
+    });
 });
