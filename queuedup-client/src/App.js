b320609c8c7a2e60cf8d9ff5c2e838c2be70e226
hliu91 <hliu91@ucsc.edu>
2020-07-02 18:45:52 +0800

    React setup

---
+++ queuedup-client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+
+//pages
+  import home from './pages/home';
+  import login from './pages/login';
+  import signup from './pages/signup';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path='/' component={home}/>
+          <Route exact path='/login' component={login}/>
+          <Route exact path='/signup' component={signup}/>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
