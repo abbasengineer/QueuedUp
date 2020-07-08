f4d78753843cce2430d26149adbeb9351c50e990
gohana <gal.ohana@gmail.com>
2020-07-08 13:16:24 -0700

    fixed error messages, formatting

--- queuedup-client/src/views/login.js
+++ queuedup-client/src/views/login.js
@@ -1,124 +1,140 @@
 import React, { Component } from "react";
-import withStyles from '@material-ui/core/styles/withStyles';
-import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
-import Grid from '@material-ui/core/Grid'; 
-import Link from 'react-router-dom/Link';
+import withStyles from "@material-ui/core/styles/withStyles";
+import PropTypes from "prop-types";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Link from "react-router-dom/Link";
+
 const styles = {
   form: {
-  textAlign : 'center'
- },
- loginTitle:{
-  margin: '2px auto 40px auto'
- },
- TextField:{
-  margin: '60px auto 12px auto'
- },
- button:{
-  margin: '80px auto 0px auto'
- },
- customError:{
-    color: 'red',
-    frontSize: '0.8rem'
- }
+    textAlign: "center",
+  },
+  loginTitle: {
+    margin: "2px auto 40px auto",
+  },
+  TextField: {
+    margin: "60px auto 12px auto",
+  },
+  button: {
+    margin: "80px auto 0px auto",
+  },
+  Error: {
+    color: "red",
+    frontSize: "0.8rem",
+  },
 };
 
 class login extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       loading: false,
-      errors: {}
-      
-    }
+      errors: {},
+    };
   }
-  handleSubmit = (event) =>{
+
+  handleSubmit = (event) => {
     event.preventDefault();
     this.setState({
-      loading: true
+      loading: true,
     });
+
     const userData = {
       email: this.state.email,
-      password: this.state.password
-    }
-    axios.post('/login', userData)
-      .then(res => {
+      password: this.state.password,
+    };
+
+    axios
+      .post("/login", userData)
+      .then((res) => {
         console.log(res.data);
         this.setState({
-          loading: false
+          loading: false,
         });
-        this.props.history.push('/');
+        this.props.history.push("/");
       })
-      .catch(err =>{
+      .catch((err) => {
         this.setState({
           errors: err.response.data,
-          loading: false
-        })
-      })
-
+          loading: false,
+        });
+      });
   };
-  handleChange = (event) =>{
+
+  handleChange = (event) => {
     this.setState({
-    [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
+
   render() {
     const { classes } = this.props;
     const { errors, loading } = this.state;
+
     return (
       <Grid container className={classes.form}>
-        <Grid item sm/>
+        <Grid item sm />
         <Grid item sm>
-          <Typography variant='h3' className={classes.loginTitle}> 
+          <Typography variant="h3" className={classes.loginTitle}>
             Welcome Back
           </Typography>
           <form noValidate onSubmit={this.handleSubmit}>
-            <TextField 
-              id="email" 
-              name="email" 
-              type="email" 
-              label="Email" 
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
               className={classes.TextField}
               helperText={errors.email}
               error={errors.email ? true : false}
-              value={this.state.email} 
-              onChange={this.handleChange} 
-              fullWidth/>
-              <TextField 
-                id="password" 
-                name="password" 
-                type="password" 
-                label="Password" 
-                className={classes.TextField}
-                helperText={errors.password}
-                error={errors.password ? true : false}
-                value={this.state.password} 
-                onChange={this.handleChange} 
-                fullWidth/>
-                {errors.general && (
-                  <Typography variant="body2" className={classes.customError}>
-                    {errors.general}
-                  </Typography>
-                )}
-      
-            <small>New to QueuedUp? <Link to ="/signup">Sign Up</Link></small>
-            <br/>
-            <Button type="submit" variant="contained" color="primary" className={classes.button}>
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.TextField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            />
+            {errors.info && (
+              <Typography variant="body2" className={classes.Error}>
+                {errors.info}
+              </Typography>
+            )}
+
+            <small>
+              New to QueuedUp? <Link to="/signup">Sign up here!</Link>
+            </small>
+            <br />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.button}>
               Log In
             </Button>
           </form>
         </Grid>
-        <Grid item sm/>
+        <Grid item sm />
       </Grid>
     );
   }
 }
+
 login.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
 };
+
 export default withStyles(styles)(login);
