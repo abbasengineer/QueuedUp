3e869e6e084be77eb3c46ff1f9b151ceeea96ec0
gohana <gal.ohana@gmail.com>
2020-07-14 20:28:48 -0700

    merge fix

--- queuedup-client/src/components/post.js
+++ queuedup-client/src/components/post.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import { Avatar } from "@material-ui/core";
 import DeletePost from "./delete-post";
 import EditPost from "./edit-post";
 import PostModal from "./post-modal";
